140dfb105373fc7839249ad38cb2d22070ccaade
AlessP93 <alespiscino@libero.it>
2022-09-23 15:46:35 +0200

    Aggiungere i file di progetto.

---
+++ Indirizzo.cs
@@ -0,0 +1,20 @@
+﻿
+public class Indirizzo
+{
+    public string Name { get; }
+    public string Surname { get; }
+    public string Street { get; }
+    public string City { get; }
+    public string Province { get; }
+    public int Zip { get; }
+
+    public Indirizzo(string name, string surname, string street, string city, string province, int zip)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        Zip = zip;
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.Contracts;
+
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+
+StreamReader indirizzi = File.OpenText("../../../addresses.csv");
+
+while (!indirizzi.EndOfStream);
+{
+    string riga = indirizzi.ReadLine();
+
+    string[] split = riga.Split(",");
+
+    Console.WriteLine(riga);
+
+    try
+    {
+        string Name = split[0];
+        string Surname = split[1];
+        string Street = split[2];
+        string City = split[3];
+        string Province = split[4];
+        int Zip = int.Parse(split[5]);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("ciao");
+    }
+}
+
+
